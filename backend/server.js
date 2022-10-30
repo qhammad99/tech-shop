d2dd650b67417f649c7fbb8fcdc2c1c97ffe2340
Hammad Qureshi <hammadahmadqureshi99@gmail.com>
2022-10-31 00:01:58 +0500

    db connection

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const app = require('./app');
 const dotenv = require('dotenv');
+const connectDatabase = require('./config/database');
 
 // env config
 const res = dotenv.config({path:'./config/config.env'});
 
+// connect database
+connectDatabase();
+
 app.listen(process.env.PORT, ()=>{
-    console.log(res);
     console.log(`Server is working on http://localhost:${process.env.PORT}`);
 });
